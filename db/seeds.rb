29671419c0d59d36840508244f90872be9165bff
yan <yan@yantekiMacBook-Air.local>
2023-08-20 00:28:23 +0800

    Add devise...

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.create(email: "test@a.com", password: 'password', password_confirmation: 'password')
 10.times do |x|
-    Post.create(title: "title #{x}", body: "body #{x} something i dont know")
+    Post.create(title: "title #{x}", body: "body #{x} something i dont know", user_id: User.first.id )
 end
